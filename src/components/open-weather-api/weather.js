c259392cd75d92340c8730757b8cb7e48d94b0ca
al <albanyaljunied@gmail.com>
2021-11-25 13:12:15 +0800

    Hiding API KEy

--- src/components/open-weather-api/weather.js
+++ src/components/open-weather-api/weather.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API_KEY = "3fd0be8ca45eb247236d8342e05526b0";
-
 export function callWeatherAPI(location) {
     return axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
     );
 }
